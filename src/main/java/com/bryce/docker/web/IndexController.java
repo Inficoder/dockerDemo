9de944ecfc8357228f9638a0a53d699459570d38
inficoder <inficoder@gmail.com>
2021-05-10 14:54:48 +0800

    init

---
+++ src/main/java/com/bryce/docker/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.bryce.docker.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class IndexController {
+
+    @RequestMapping("")
+    public String index(){
+        return "miao!";
+    }
+
+}
